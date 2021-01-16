ad7daecd3aa21f349c39e7225b4b9cc2855719e4
Gustav <gkhaavik@gmail.com>
2021-01-16 03:35:35 +0100

    Init project

---
+++ src/main/java/com/wyvencraft/respawnblocks/RespawnBlocks.java
@@ -0,0 +1,30 @@
+package com.wyvencraft.respawnblocks;
+
+import com.wyvencraft.wyvencore.WyvenCore;
+import com.wyvencraft.wyvencore.addons.Addon;
+
+public class RespawnBlocks extends Addon {
+    public RespawnBlocks(WyvenCore plugin) {
+        super(plugin);
+    }
+
+    @Override
+    public void onLoad() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public void reloadConfig() {
+
+    }
+}
